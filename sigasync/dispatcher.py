ff8757b3a997bfed97fbcb26bef1ce8631762c1c
Edward George <ed@woome.com>
2009-02-16 20:04:14 +0000

    add possible setting for disabling the spooler handling of the queue stuff

--- sigasync/dispatcher.py
+++ sigasync/dispatcher.py
@@ -2,7 +2,9 @@ from django.dispatch import dispatcher
 from sigasync.sigasync_handler import sigasync_handler as handler
 
 def async_connect(func, **kwargs):
-    dispatcher.connect(handler(func), **kwargs)
+    if not getattr(settings, 'DISABLE_SIGASYNC_SPOOL', False):
+        func = handler(func)
+    dispatcher.connect(func, **kwargs)
 
 #END
 
