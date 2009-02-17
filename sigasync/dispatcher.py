f6b428fb723a11960b04ec10154c5feca3bc096e
Edward George <ed@woome.com>
2009-02-17 02:22:41 +0000

    non-weak ref signal receiver 'hack'

--- sigasync/dispatcher.py
+++ sigasync/dispatcher.py
@@ -5,7 +5,7 @@ from sigasync.sigasync_handler import sigasync_handler as handler
 def async_connect(func, **kwargs):
     if not getattr(settings, 'DISABLE_SIGASYNC_SPOOL', False):
         func = handler(func)
-    dispatcher.connect(func, **kwargs)
+    dispatcher.connect(func, weak=False, **kwargs)
 
 #END
 
