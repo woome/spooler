12f31f1957e80ec035467041b2e0be82ff628adb
io2@host55 <none@none>
2010-06-25 04:20:04 -0700

    backing out changes that chould not have been commited

--- sigasync/http.py
+++ sigasync/http.py
@@ -1,9 +1,6 @@
 from webserviceapp import webservice
 from sigasync.sigasync_spooler import SigAsyncSpool
-import logging
 
 class HttpSpool(SigAsyncSpool):
     def submit(self, spec):
-        logger = logging.getLogger("yesmail.http.submit")
-        logger.warning("submitting job to /spooler/%s/" % self._name)
         webservice.post('/spooler/%s/' % self._name, spec)
